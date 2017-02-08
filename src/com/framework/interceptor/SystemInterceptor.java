b510d2a39808126a5a210398d11c519a1a0afa05
SW-zhang <zhangshuwang1990@163.com>
2017-02-08 14:47:07 +0800

    commit

--- src/com/framework/interceptor/SystemInterceptor.java
+++ src/com/framework/interceptor/SystemInterceptor.java
@@ -1,5 +1,6 @@
 package com.framework.interceptor;
 
+import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -15,6 +16,8 @@ public class SystemInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String url = request.getRequestURI();
+        log.info("[" + url + "]---[params:" + JSON.toJSONString(request.getParameterMap()) + "]");
 
         System.out.println("you can do something");
 
